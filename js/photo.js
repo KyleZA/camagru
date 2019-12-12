b093d138c9f844094e02a7e80cd35ce833444ad0
KyleZA <dlg.pein@gmail.com>
2019-12-12 08:30:06 +0200

    camera working to take images

---
+++ js/photo.js
@@ -0,0 +1,50 @@
+(function() {
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var video = document.getElementById('video');
+    var img;
+    var videoflag = 0;
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({
+            video: true 
+        }).then(function(stream) {
+            video.srcObject = stream;
+            video.play();
+        });
+    }
+    function chooseimg(){
+        var choose = document.querySelectorAll(".imgsrc");
+    
+        choose.forEach(function(element){
+            element.addEventListener("click",function(){
+            img = element;
+            if (img && videoflag === 1){
+                if (img.src === "http://localhost:8080/Camagru/stickers/frame1.png"){
+                    context.drawImage(img, 75, 25, 250, 250);
+                }
+                else if (img.src === "http://localhost:8080/Camagru/stickers/frame2.png"){
+                    context.drawImage(img, 60, 100, 100, 100);
+                }
+                else if (img.src === "http://localhost:8080/Camagru/stickers/frame3.png"){
+                    context.drawImage(img, 250, 125, 100, 100);
+                }
+                else if (img.src === "http://localhost:8080/Camagru/stickers/frame4.png"){
+                    context.drawImage(img, 0, 0, 400, 300);
+                }
+                else if (img.src === "http://localhost:8080/Camagru/stickers/frame5.png"){
+                    context.drawImage(img, 65, 114, 250, 250);
+               }
+               context.drawImage(img, 75, 25, 250, 250);
+                // var dataURL = canvas.toDataURL('image/png');
+                // document.getElementById("imgsrc").value = dataURL;
+            }
+        });
+    });}
+    
+    chooseimg();
+    document.getElementById("snap").addEventListener("click", function() {
+        context.drawImage(video, 0, 0, 640, 400);
+        videoflag = 1;
+    });
+})();
+//draw
